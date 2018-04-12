bd562508e8e84dbf372285af9d1749a0c2ebc73c
Steve Jacobs <sjacobs@truefit.io>
2018-04-12 09:03:11 -0400

    simplified createTabNavigator method

--- src/navigators/createTabNavigator.js
+++ src/navigators/createTabNavigator.js
@@ -3,10 +3,5 @@ import _ from 'lodash';
 import {TabNavigator, StackNavigator} from 'react-navigation';
 
 export const createTabNavigator = config => {
-  const tabRoutes = _.reduce(config.routes, (result, route, key) => ({
-    ...result,
-    [key]: {screen: route.screen || StackNavigator(route.stack, {initialRouteName: route.initialRouteName})},
-  }), {});
-
-  return TabNavigator(tabRoutes, config.config);
+  return TabNavigator(config.routes, config.config);
 };
